e4e10f0f4f5e9cfef56952640f040974d6cac296
Wladimir Duran <wladimir.duran@usach.cl>
2020-05-13 01:15:00 -0400

    Agregado TDA de las distintas zonas en el nivel de representacion

--- TDA.rkt
+++ TDA.rkt
@@ -6,17 +6,40 @@
 
 ;Zonas
 
+
+
 ;Workspace
 ;Lista de nombres de archivos
-;Ej: '(Archivo1.rkt, Archivo2.rkt, README.txt)
+;Ej: '("Archivo1.rkt", "Archivo2.rkt", "README.txt")
+
+;Index
+;Lista de nombres de archivos
+;Ej: '("Archivo1.rkt", "Archivo2.rkt", "README.txt")
+
+;Local Repository
+;Lista de nombres de archivos
+;Ej: '("Archivo1.rkt", "Archivo2.rkt", "README.txt")
+
+;Commit
+;Lista con id, fecha, persona que hizo el commit y los nombres de los archivos que 
+;Ej '(3456, '(23, 03, 18), "Juan Perez", '("Archivo1.rkt", "README.txt"))
+
+;Remote repository
+;Lista de listas con commits, agregando el id del commit padre de este
+;Ej '('('(3456, 4567), '(23, 03, 18), "Juan Perez", '("Archivo1.rkt", "README.txt")), '('(4567, 0), '(20, 03, 18), "Carlos Santana", '("Archivo1.rkt", "README.txt")))
 
 
 
 ;Constructores
 
-;Crea una lista con un archivo llamado README
+;Crea una lista con un archivo llamado README.txt, que contara como la zona workspace
+;No hay input
+;Sale una lista con un elemento llamado README.txt
 (define crearWorkspace
     (lambda ()
         (list "README.txt")
     )
 )
+
+
+
